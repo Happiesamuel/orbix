75fe6162260ade28b9498107fc2d852251ef329e
Hs-the-dev <odioneghogho@gmail.com>
2025-06-24 10:32:40 +0100

    seconf

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { FaOpencart, FaRegHeart } from "react-icons/fa";
-
+import { Input } from "../ui/input";
+import { IoSearch } from "react-icons/io5";
 export default function Header() {
   return (
     <div className="flex items-center justify-between">
       <h1>
         BR.<span>F</span>
       </h1>
-      <input />
+      <div className="flex items-center gap-2 justify-center px-3 w-[280px] rounded-md text-[#a4a4a4] bg-[#f2f0ea]">
+        <IoSearch className="text-2xl"/>
+        <Input className="border-none shadow-none p-0 placeholder:text-[#a4a4a4] text-[#010101]" placeholder="Search"/>
+      </div>
       <div className="flex items-center gap-2">
         <div className="flex items-center flex-col gap-1">
           <FaOpencart />
@@ -15,9 +19,9 @@ export default function Header() {
         </div>
         <div className="flex items-center flex-col gap-1">
           <FaRegHeart />
-          <p>Cart</p>
+          <p>Favorite</p>
         </div>
-        immage
+        image
       </div>
     </div>
   );
